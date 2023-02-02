48da3f128370870dd31a3a199d52d26c7de84088
giovannagadelha <117370354+giovannagadelha@users.noreply.github.com>
2023-02-02 11:24:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//let entrada=parseInt(prompt("digite o ano que você nasceu:"))
+//alert(2023+entrada)
+//alert(typeof entrada)
+
+/*let section=document.querySelector("section")
+
+
+    function toDark(){
+        section.classList.add("dark")
+            section.classList.remove("light")
+}
+    function toLight(){
+        section.classList.add("light")
+            section.classList.remove("dark")
+}*/
+
+$(document).ready(function() {
+    $("#color_mode").on("change", function () {
+        colorModePreview(this);
+    })
+});
+
+function colorModePreview(ele) {
+    if($(ele).prop("checked") == true){
+        $('body').addClass('dark-preview');
+        $('body').removeClass('white-preview');
+    }
+    else if($(ele).prop("checked") == false){
+        $('body').addClass('white-preview');
+        $('body').removeClass('dark-preview');
+    }
+}
